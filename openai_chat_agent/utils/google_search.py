d7e79c0c92c63d2f0646eb5939a4296b018becf3
karencfisher <karencfisher@hotmail.com>
2023-07-07 20:02:24 -0600

    revert to synchrnous web scraping

--- openai_chat_agent/utils/google_search.py
+++ openai_chat_agent/utils/google_search.py
@@ -4,9 +4,8 @@ from bs4 import BeautifulSoup
 import re
 import os
 import numpy as np
-import asyncio
-import faiss
 import openai
+import time
 
 from dotenv import load_dotenv
 from collections import defaultdict
@@ -15,11 +14,6 @@ from fake_useragent import UserAgent
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
-
-try:
-    from openai_chat_agent.utils.async_web_scraper import AsyncWebScraper
-except:
-    from utils.async_web_scraper import AsyncWebScraper
 
 
 class GoogleSearch:
@@ -54,44 +48,35 @@ class GoogleSearch:
             return [{"error_response": response.status_code}]
         return response.json()['items']
     
-    async def __get_documents_async(self, items):
+    def __get_documents(self, items):
         if self.verbose:
             print('Fetching pages and breaking into chunks')
+        docs = []
+        for item in items:
+            # spoof as a chrome browser and fetch website
+            ua = UserAgent
+            header = {'User-Agent': str(ua.chrome)}
+            article_response = requests.get(item['link'], headers=header)
 
-        scraper = AsyncWebScraper(self.text_splitter)
-        docs = await scraper.get_documents(items)
+            # scrape the text
+            soup = BeautifulSoup(article_response.text, 'html.parser')
+            text = soup.get_text().strip()
+            text = re.sub(r'\n+', '\n', text)
 
+            # split into documents for semantic search
+            docs += self.text_splitter.create_documents([text], 
+                        metadatas=[{'reference': (item['title'], item['link'])}])
+            if self.verbose:
+                print(f'\r{len(docs)} documents', end='')
+            
         if self.verbose:
             print('')
-        return docs
-    
-    def __get_documents(self, items):
-        # if self.verbose:
-        #     print('Fetching pages and breaking into chunks')
-        # docs = []
-        # for item in items:
-        #     # spoof as a chrome browser and fetch website
-        #     ua = UserAgent
-        #     header = {'User-Agent': str(ua.chrome)}
-        #     article_response = requests.get(item['link'], headers=header)
-
-        #     # scrape the text
-        #     soup = BeautifulSoup(article_response.text, 'html.parser')
-        #     text = soup.get_text().strip()
-        #     text = re.sub(r'\n+', '\n', text)
-
-        #     # split into documents for semantic search
-        #     docs += self.text_splitter.create_documents([text], 
-        #                 metadatas=[{'reference': (item['title'], item['link'])}])
-            
-        #     if self.verbose:
-        #         print(f'\r{len(docs)} documents', end='')
-
-        loop = asyncio.get_event_loop()
-        return loop.run_until_complete(self.__get_documents_async(items))
+        return docs 
     
     def __store_documents(self, docs):
         # vectorize documents and select best k_best
+        if self.verbose:
+            print(f'Create and load documents to vector DB')
         if self.db is None:
             self.db = FAISS.from_documents(documents=docs, embedding=self.embeddings)
         else:
@@ -99,6 +84,8 @@ class GoogleSearch:
             self.db.merge_from(temp_db)
 
     def __get_selections(self, query):
+        if self.verbose:
+            print('Semantic search on vector DB')
         # vectorize documents and select best k_best
         selections = self.db.similarity_search_with_score(query, k=self.k_best)
         if self.verbose:
@@ -109,6 +96,8 @@ class GoogleSearch:
         return selections
     
     def __get_summary(self, selections):
+        if self.verbose:
+            print('Summarizing findings.')
         # get references used, in ranked order
         hash = defaultdict(int)
         for selection in selections:
@@ -127,21 +116,58 @@ class GoogleSearch:
     def __call__(self, query):
         # if we have data, see if query is valid for it
         if self.db is not None:
-            selections = self.__get_selections(query)
+            if self.verbose:
+                selections = self.__timeit(self.__get_selections, (query,))
+            else:
+                selections = self.__get_selections(query)
             # if distance < threshold, we can use those results
             if selections[0][1] < self.l2_threshold:
                 if self.verbose:
                     print('Re-using previous search data')
+                    return self.__timeit(self.__get_summary, (selections,))
                 return self.__get_summary(selections)
             else:
                 if self.verbose:
                     print('Previous data is not sufficient, so I have to search again.')
                 
         # if not, we perform a new search
-        items = self.__get_pages(query)
-        docs = self.__get_documents(items)
-        self.__store_documents(docs)
-        selections = self.__get_selections(query)
-        return self.__get_summary(selections)
+        if self.verbose:
+            items = self.__timeit(self.__get_pages, (query,))      
+            docs = self.__timeit(self.__get_documents, (items,))
+            self.__timeit(self.__store_documents, (docs,))
+            selections = self.__timeit(self.__get_selections, (query,))
+            return self.__timeit(self.__get_summary, (selections,))
+        else:
+            items = self.__get_pages(query)       
+            docs = self.__get_documents(items)
+            self.__store_documents(docs)
+            selections = self.__get_selections(query)
+            return self.__get_summary(selections)
+    
+    def __timeit(self, func, args):
+        start = time.time()
+        output = func(*args)
+        elapsed = time.time() - start
+        print(f'time for this step = {elapsed: .3f} seconds')
+        return output
 
     
+def test():
+    search = GoogleSearch(verbose=True)
+
+    done = False
+    while not done:
+        query = input('==> ')
+        if query == 'goodbye':
+            done = True
+        else:
+            summary, references = search(query)
+            print(f'I enquired:\n\"{query}\"\n')
+            print(f'I learned:\n{summary}\n')
+            print(f'My references:')
+            for reference in references:
+                title, link = reference
+                print(f'{title}: {link}')
+
+if __name__ == '__main__':
+    test()
